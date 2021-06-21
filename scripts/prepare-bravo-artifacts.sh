55c906f1f9a52ee916fa9853705de420fb4d4e71
Marko Dakic <marko.dakic@bravosystems.com>
2021-06-21 14:29:41 +0200

    Synced helm chart and increased default version for bravo artifact.

--- scripts/prepare-bravo-artifacts.sh
+++ scripts/prepare-bravo-artifacts.sh
@@ -1,11 +1,11 @@
 
 
 export REGISTRY=${KUBEFED_CONTAINER_REGISTRY:-harbor.central.dev.didevops.com/platform}
-export GIT_VERSION=${KUBEFED_CONTAINER_TAG:-0.7.0.pes1}
+export GIT_VERSION=${KUBEFED_CONTAINER_TAG:-0.7.0.pes2}
 
 make build container
 
-docker push $REGISTRY:$GIT_VERSION
+docker push "$REGISTRY/kubefed:$GIT_VERSION"
 
 ./scripts/sync-up-helm-chart.sh
 
